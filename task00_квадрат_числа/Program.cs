690586a8811cd56b5256d53b6e6ebe2c80065e5a
NadiSaVa <1986creeta@gmail.com>
2023-10-07 17:36:46 +0300

    ешила превые три задачи

---
+++ task00_квадрат_числа/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+0. Напиши программу, котрая на вход принимает число
+и выдает его квадрат (число умножженное на само себя)
+*/
+/*Прошу число у пользователя*/
+System.Console.WriteLine("Введите число: "); //cw- быстрый вызов Console.WriteLine
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = userNumber * userNumber;
+Console.WriteLine(result);
+System.Console.WriteLine(result);
---
+++ task01_квадрат_введеного_числа/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+1. Напиши программу воторая принимает на вход два числа
+и проверяет явяется ли первое число квадратом второго
+*/
+System.Console.WriteLine("Ведите числа: ");
+int userNumb1 = Convert.ToInt32(Console.ReadLine());
+int userNumb2 = Convert.ToInt32(Console.ReadLine());
+
+/* int sqNumb2 = userNumb2 * userNumb2;
+bool result = userNumb1 == sqNumb2;
+
+Console.WriteLine(result);
+*/
+// А вот второй вариант
+if(userNumb1 == userNumb2 * userNumb2)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
