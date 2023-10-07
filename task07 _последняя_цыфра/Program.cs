43dd64ac6b4780433b1d673ac8c3f9293557070b
NadiSaVa <1986creeta@gmail.com>
2023-10-07 22:58:47 +0300

    Задача07

---
+++ task07 _последняя_цыфра/Program.cs
@@ -0,0 +1,8 @@
+﻿/* 7. Напишите программу, которая принимает на вход трехзначноечисло
+ и на выходе паказывает последнюю цыфру этого числа
+*/
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = userNumber % 10;
+System.Console.WriteLine(result);
