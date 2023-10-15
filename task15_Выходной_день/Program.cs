998a5264faa84bcfab88749c8108c0ef5fbab101
NadiSaVa <1986creeta@gmail.com>
2023-10-16 00:42:00 +0300

    сделала домашки

---
+++ task15_Выходной_день/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+*/
+System.Console.WriteLine("Ведите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day <= 5 && day > 0)
+{
+    Console.WriteLine("Это рабочий день!");
+}
+if(day == 6 || day == 7)
+{
+    Console.WriteLine("Ура выходной!");
+}
+else if(day < 1 || day > 7)
+{
+    Console.WriteLine("Такого дня не существует!");
+}
+
+
