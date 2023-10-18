fa9e5c4bce3feb221248ed939488383a938c010a
NadiSaVa <1986creeta@gmail.com>
2023-10-18 09:28:28 +0300

    удалили решение задачи в 19

--- task19_Полиндром/Program.cs
+++ task19_Полиндром/Program.cs
@@ -3,14 +3,3 @@
 пятизначное число и проверяет, 
 является ли оно палиндромом.
 */
-System.Console.Write("Введите координату в четверти: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите координату в четверти: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите координату в четверти: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите координату в четверти: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-double d = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)), 2);
-System.Console.WriteLine(d);
