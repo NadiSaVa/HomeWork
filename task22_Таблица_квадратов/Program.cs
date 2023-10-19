36284f013658382b1a664b64ab3ca87b42c4f6d0
NadiSaVa <1986creeta@gmail.com>
2023-10-19 15:15:30 +0300

    задача 22 решена с валерией

--- task22_Таблица_квадратов/Program.cs
+++ task22_Таблица_квадратов/Program.cs
@@ -3,8 +3,30 @@
 выдаёт таблицу квадратов чисел от 1 до N.
 */
 
+/*
+На семинаре
 System.Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for(int i = 1; i <= n; i++)
     Console.Write(Math.Pow(i, 2) + " ");
-    
+*/
+
+// А вот это с Валерией
+
+/*System.Console.Write("Введите число: ");
+int userNumb = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= userNumb; i++)
+{
+    System.Console.WriteLine($"(i) * (i) = (Math.Pow(i, 2))");
+}
+*/
+
+// А вот правильное решение
+System.Console.WriteLine("Enter number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+   System.Console.WriteLine($"{i} * {i} * {i} = {Math.Pow(i, 3)}");
+}
