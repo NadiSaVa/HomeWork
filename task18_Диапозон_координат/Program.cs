f6d1c9a56466c52d60ea481d77661e80bcf2c8e2
NadiSaVa <1986creeta@gmail.com>
2023-10-19 14:33:00 +0300

    задача 18 решена с валерией

--- task18_Диапозон_координат/Program.cs
+++ task18_Диапозон_координат/Program.cs
@@ -3,6 +3,9 @@
 показывает диапазон 
 возможных координат точек в этой четверти (x и y).
 */
+/*
+Задача решена на семинаре
+
 string f(int a)
 {
    if (a == 1)
@@ -16,3 +19,24 @@ string f(int a)
 System.Console.Write("Введите номер четверти: ");
 int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(f(x));
+*/
+
+System.Console.WriteLine("Введите номер четверти: ");
+int userCvoter = Convert.ToInt32(Console.ReadLine());
+
+if (userCvoter == 1)
+{
+    System.Console.WriteLine("Все x и y больше 0");
+}
+if (userCvoter == 2)
+{
+    System.Console.WriteLine("Все x меньше 0, y больше 0");
+}
+if (userCvoter == 3)
+{
+    System.Console.WriteLine("Все x и y меньше 0");
+}
+if (userCvoter == 4)
+{
+    System.Console.WriteLine("Все x больше 0, y меньше 0");
+}
