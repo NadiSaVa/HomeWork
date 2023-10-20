ef7a1b8dc07fe1e5e94d059d2840a8535a76cfa5
NadiSaVa <1986creeta@gmail.com>
2023-10-20 16:42:07 +0300

    задача 26  решена

---
+++ task26_Количество_цифр_в_числе/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+*/
+
+
+System.Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Result(int number)
+{
+    int count = 0;
+    for (int i = number; i > 0; i /= 10)
+    {
+        count = count + 1;
+    }
+    return count;
+}
+
+int userCount = Result(userNumber);
+System.Console.WriteLine(userCount);
+
