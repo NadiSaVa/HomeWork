5ea9e11f2be15ed371df32212fb769f2c2e75046
NadiSaVa <1986creeta@gmail.com>
2023-10-20 16:12:20 +0300

    задача 24  решена

---
+++ task24_Сумма_от_1_до_А/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+*/
+/* Вариант 1
+System.Console.Write("Введите число А: ");
+
+int A = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+for (int i = 1; i <= A; i++)
+{
+    summ = summ + i;
+}
+System.Console.WriteLine(summ);
+*/
+
+// Второй вариант
+System.Console.Write("Введите число А: ");
+int userA = Convert.ToInt32(Console.ReadLine());
+
+int Num(int a)
+{
+    int summ = 0;
+    for (int i = 1; i <= userA; i++)
+    {
+        summ = summ + i;
+    }
+    return summ;
+}
+int userResult = Num(userA);
+System.Console.WriteLine(userResult);
+
+
