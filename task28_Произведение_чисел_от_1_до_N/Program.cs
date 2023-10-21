0a81db54d928965bb26c50e884929c5fb8a06520
NadiSaVa <1986creeta@gmail.com>
2023-10-21 13:18:29 +0300

    задача 28  решена

---
+++ task28_Произведение_чисел_от_1_до_N/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход число N и 
+выдаёт произведение чисел от 1 до N.
+*/
+
+System.Console.Write("Введите число А: ");
+int userA = Convert.ToInt32(Console.ReadLine());
+
+int Num(int a)
+{
+    int summ = 1;
+    for (int i = 1; i <= userA; i++)
+    {
+        summ = summ * i;
+    }
+    return summ;
+}
+int userResult = Num(userA);
+System.Console.WriteLine(userResult);
