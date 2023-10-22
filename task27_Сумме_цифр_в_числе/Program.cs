a07f7c8018bfc46e2adc6d4df9cee28228c89631
NadiSaVa <1986creeta@gmail.com>
2023-10-22 22:10:18 +0300

    задача 27  решена

---
+++ task27_Сумме_цифр_в_числе/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+*/
+
+Console.Clear();
+
+int SumNumber(int a)
+{
+    int result = 0;
+    for (int i = a; i > 0; i /= 10)
+    {
+        result = result + i % 10;
+    }
+    return result;
+}
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int userRes = SumNumber(num);
+System.Console.WriteLine(userRes);
+
+
