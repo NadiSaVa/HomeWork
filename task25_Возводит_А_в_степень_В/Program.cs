f7ede6f942572265b69905fe938bb2aa8b947f60
NadiSaVa <1986creeta@gmail.com>
2023-10-22 21:16:14 +0300

    задача 25  решена c циклами

--- task25_Возводит_А_в_степень_В/Program.cs
+++ task25_Возводит_А_в_степень_В/Program.cs
@@ -9,10 +9,23 @@ int numA = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите число B: ");
 int numB = Convert.ToInt32(Console.ReadLine());
 
+/*
 void NatStep(int a, int b)
 {
     double result = Math.Pow(a, b);
     System.Console.WriteLine(result);
 }
 NatStep(numA, numB);
+*/
 
+// решение с циклами
+void NatStep(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    System.Console.WriteLine(result);
+}
+NatStep(numA, numB);
