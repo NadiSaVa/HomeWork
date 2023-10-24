6103fd3085249a3f4711874d524e78b3ac2f8c5f
NadiSaVa <1986creeta@gmail.com>
2023-10-24 21:10:39 +0300

    задача 33  решена

---
+++ task33_присутствие_заданного_числа/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Задайте массив. Напишите программу, которая определяет, 
+присутствует ли заданное число в массиве
+*/
+
+Console.Clear();
+
+int TakeEntNum(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] GetArray(int start, int end, int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if (i < array.Length - 1)
+        System.Console.Write(", ");  
+    }
+    System.Console.WriteLine("]");
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -1 * array[i];
+        System.Console.Write(array[i]);
+        if (i < array.Length - 1)
+        System.Console.Write(", ");
+    }
+    System.Console.WriteLine("]");
+}
+
+bool findNum(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(num == array[i])
+        return true;
+    }
+    return false;
+}
+
+int userArraySize = TakeEntNum("Введите размер массива: ");
+int userArrayStart = TakeEntNum("Введите начало массива: ");
+int userArrayEnd = TakeEntNum("Введите конец массива: ");
+
+int[] userArray = GetArray (userArrayStart, userArrayEnd, userArraySize);
+PrintArray (userArray);
+
+int userNum = TakeEntNum("Какое число ищем ");
+System.Console.WriteLine(findNum(userArray, userNum));
