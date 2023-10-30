3b2cd844a2cf4ff6b1e863e138695fb28bc2facf
NadiSaVa <1986creeta@gmail.com>
2023-10-30 22:33:44 +0300

    задача 40 решена

---
+++ task40_Треугольник_со_сторонами_такой_длины/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник с сторонами такой длины.
+*/
+
+int TakeEntNum(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+bool Triagle(int a, int b, int c)
+{
+    bool result = false;
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        result = true;
+    }
+    return result;
+}
+
+int userA = TakeEntNum("Введите число А: ");
+int userB = TakeEntNum("Введите число B: ");
+int userC = TakeEntNum("Введите число C: ");
+System.Console.WriteLine(Triagle(userA, userB, userC));
+
+
