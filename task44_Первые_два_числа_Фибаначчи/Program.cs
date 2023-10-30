12f5ffae30f8e331ed1d5750816874eca736104a
NadiSaVa <1986creeta@gmail.com>
2023-10-30 23:35:23 +0300

    задача 44 решена

---
+++ task44_Первые_два_числа_Фибаначчи/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+Console.Write("Введите количество чисел Фибоначчи: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 1)
+{
+Console.WriteLine("Пожалуйста, введите положительное число.");
+return;
+}
+
+int first = 0;
+int second = 1;
+
+Console.Write("Первые " + n + " чисел Фибоначчи: ");
+
+if (n >= 1)
+{
+Console.Write(first);
+}
+
+if (n >= 2)
+{
+Console.Write(", " + second);
+}
+
+for (int i = 3; i <= n; i++)
+{
+int next = first + second;
+Console.Write(", " + next);
+first = second;
+second = next;
+}
+
+Console.WriteLine();
