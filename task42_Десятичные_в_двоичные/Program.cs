94757d5fea3b60cc570440b2a82662ef93c13939
NadiSaVa <1986creeta@gmail.com>
2023-10-30 23:10:36 +0300

    задача 42 решена.1

--- task42_Десятичные_в_двоичные/Program.cs
+++ task42_Десятичные_в_двоичные/Program.cs
@@ -27,3 +27,21 @@ int Conv(int number)
 }
 int uNum = TakeEntNum("Введите число: ");
 System.Console.WriteLine(Conv(uNum));
+
+/* Второй вариант
+
+string Conv(int number)
+{
+    string result = "";
+    int tmp = 0;
+    for (int i = number; i > 0; i/=2)
+    {
+        tmp = i % 2;
+        result = tmp + result;
+    }
+    return result;
+}
+
+int uNum = UserInOut("Введите число: ");
+System.Console.WriteLine(Conv(uNum));
+*/
