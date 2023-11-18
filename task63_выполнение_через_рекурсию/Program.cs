587f301c71e90a695307ac882899579a6ed14589
NadiSaVa <1986creeta@gmail.com>
2023-11-18 20:13:27 +0300

    задача 63 решена

---
+++ task63_выполнение_через_рекурсию/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+63. Выполнение через рекурсию (функция вызывает саму себя)
+
+Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void intN(int number, int current = 1)
+{
+if (number < current) return;
+else
+{
+System.Console.Write(current + " ");
+intN(number, current + 1);
+}
+}
+
+intN(number);
